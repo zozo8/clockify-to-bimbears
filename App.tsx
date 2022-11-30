7dafeb01d49aadc6a57ef66f4e90bcdb436dfe73
zozo8 <50959052+zozo8@users.noreply.github.com>
2022-11-30 16:11:07 +0100

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,48 @@
 import * as React from 'react';
 import './style.css';
+import { OutTable, ExcelRenderer } from 'react-excel-renderer';
+
+class ClockifyToTimeSheet extends React.Component {
+  state = {
+    cols: [],
+    rows: [],
+  };
+
+  fileHandler = (event) => {
+    let fileObj = event.target.files[0];
+
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({
+          cols: resp.cols,
+          rows: resp.rows,
+        });
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="file"
+          onChange={this.fileHandler.bind(this)}
+          style={{ padding: '10px' }}
+        />
+        <OutTable
+          data={this.state.rows}
+          columns={this.state.cols}
+          tableClassName="ExcelTable2007"
+          tableHeaderRowClass="heading"
+        />
+      </div>
+    );
+  }
+}
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return <ClockifyToTimeSheet />;
 }
